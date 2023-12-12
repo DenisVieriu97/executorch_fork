37e4ace40215635fe14a94a357b41e297b1e4749
Denis Vieriu <dvieriu@apple.com>
2023-12-12 14:55:01 -0800

    Fix CI

--- .ci/scripts/setup-macos.sh
+++ .ci/scripts/setup-macos.sh
@@ -118,9 +118,9 @@ print_cmake_info() {
 install_buck
 install_pip_dependencies
 
-print_cmake_info
+# print_cmake_info
 # install_pytorch_and_domains
-# install_flatc_from_source
+install_flatc_from_source
 install_executorch
 build_executorch_runner "${BUILD_TOOL}"
 
